f9fad6f9d74297b5c83abb25a9450a77d1618824
DebianJoe <jbpaladin27@gmail.com>
2013-10-22 04:18:42 -0500

    compact colors

---
+++ colors.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# <esc>[..m sequences set color
+echo "                  ANSI TERMINAL COLOR TABLE                        "
+echo -e "\033[30;1m bl \033[31;1m rd \033[32;1m grn \033[33;1m yel \033[34;1m blu \033[35;1m pur \033[36;1m cyn \033[37;1m wht \033[0m"
+echo -e "\033[40;1m bl \033[41;1m rd \033[42;1m grn \033[43;1m yel \033[44;1m blu \033[45;1m pur \033[46;1m cyn \033[47;1m wht \033[0m"
+echo -e "\033[30;2m bl \033[31;2m rd \033[32;2m grn \033[33;2m yel \033[34;2m blu \033[35;2m pur \033[36;2m cyn \033[37;2m wht \033[0m"
+echo -e "\033[40;2m bl \033[41;2m rd \033[42;2m grn \033[43;2m yel \033[44;2m blu \033[45;2m pur \033[46;2m cyn \033[47;2m wht \033[0m"
+echo -e "\033[30;4m bl \033[31;4m rd \033[32;4m grn \033[33;4m yel \033[34;4m blu \033[35;4m pur \033[36;4m cyn \033[37;4m wht \033[0m"
+echo -e "\033[40;4m bl \033[41;4m rd \033[42;4m grn \033[43;4m yel \033[44;4m blu \033[45;4m pur \033[46;4m cyn \033[47;4m wht \033[0m"
+echo -e "\033[30;5m bl \033[31;5m rd \033[32;5m grn \033[33;5m yel \033[34;5m blu \033[35;5m pur \033[36;5m cyn \033[37;5m wht \033[0m"
+echo -e "\033[40;5m bl \033[41;5m rd \033[42;5m grn \033[43;5m yel \033[44;5m blu \033[45;5m pur \033[46;5m cyn \033[47;5m wht \033[0m"
+echo -e "\033[30;7m bl \033[31;7m rd \033[32;7m grn \033[33;7m yel \033[34;7m blu \033[35;7m pur \033[36;7m cyn \033[37;7m wht \033[0m"
+echo -e "\033[40;7m bl \033[41;7m rd \033[42;7m grn \033[43;7m yel \033[44;7m blu \033[45;7m pur \033[46;7m cyn \033[47;7m wht \033[0m"
